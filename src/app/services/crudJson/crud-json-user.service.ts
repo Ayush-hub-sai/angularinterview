49c6e02b0233cc8ff40087adbc4b1c8bed58d57d
Bishnu Behera <bishnu.glintlets@gmail.com>
2024-08-29 14:14:30 +0530

    added json server api

--- src/app/services/crudJson/crud-json-user.service.ts
+++ src/app/services/crudJson/crud-json-user.service.ts
@@ -6,6 +6,16 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class CrudJsonUserService {
+  //1- npm install -g json-server
+
+  //2-Create a db.json file in your project root with the following content:
+  // json
+  // Copy code
+  // {
+  //   "users": []
+  // }
+
+  //3- json-server --watch db.json
 
   private apiUrl = 'http://localhost:3000/users';
 
